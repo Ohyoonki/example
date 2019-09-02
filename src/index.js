0e48b855b839a23b92eed1b46d17cf7f4ee13fbb
Ohyoonki <gkdldn233@gmail.com>
2019-09-02 19:57:51 +0900

    1주차

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require("express")
+const_ = require("lodash");
 const app = express();
-let users = [];
+let users = [{
+    id: 1,
+    name: "홍길동"
+},{
+    id: 2,
+    name: "강철수"
+}];
 let user = null;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -12,24 +19,34 @@ app.get("/",(req,res) => {
 });
 
 app.get("/users",(req,res) => {
-  if(user){
-        res.send("user " +user.name+" get");
-    }else{
-        res.send("유저가 존재하지 않습니다.");
-        }
+    let msg = "유저가 존재하지 않습니다.";
+  if(users.length > 0){
+        msg = users.length+"명의 유저가 존재합니다.";
+    }
+        res.send({msg, result: users});
+        
 });
 
 app.get("/users/:id",(req,res) => {
-    if(user && user.id == req.params.id){
-        res.send("user "+user.name+" get");
+    let msg = "id가 "+req.params.id+"인 유저가 존재하지 않습니다.";
+    let user = _.find(users,["id",parseInt(req.params.id)]);
+    if(user){
+        msg = "성공적으로 조회하였습니다.";
     }
    
-    res.send("user id" +req.params.id+"가 존재하지 않습니다.");
+    res.send({msg,result: user});
 });
 
 app.post("/users",(req,res) => {
-    user = req.body
-    res.send("user add" +user.name);
+    const check_user = _.find(users, ["id", req.body.id]);
+    let msg = req.body.id+"아이디를 가진 유저가 이미 존재합니다.";
+    let success = false;
+    if(!check_user){
+        users.push(req.body);
+        msg = req.body.name+"유저가 추가되었습니다.";
+        success = true;
+    }
+    res.send({msg, success});
 });
 
 app.put("/users/:id",(req,res) => {
@@ -45,7 +62,7 @@ app.delete("/users/:id",(req,res) => {
         user =null;
         res.send("user" +req.params.id+" delete");
     }else{
-        res.send("user id "+req.params.id+"가 존재하지 않습니다")
+        res.send("user id "+req.params.id+"가 존재하지 않습니다");
     }
   
     
