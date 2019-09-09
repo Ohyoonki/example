c6d8c2185d82812156ddec23673f3a3cd3714018
Ohyoonki <gkdldn233@gmail.com>
2019-09-09 15:56:44 +0900

    2주차 boards

--- src/index.js
+++ src/index.js
@@ -1,70 +1,19 @@
-const express = require("express")
-const_ = require("lodash");
+const express = require("express");
 const app = express();
-let users = [{
-    id: 1,
-    name: "홍길동"
-},{
-    id: 2,
-    name: "강철수"
-}];
-let user = null;
+const user_router = require("./route/users");
+const board_router = require("./route/boards");
+
+
+
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use("/users", user_router);
+app.use("/boards", board_router);
+
 app.listen(3000);
 
-app.get("/",(req,res) => {
-   
-    res.send("hi")
-});
-
-app.get("/users",(req,res) => {
-    let msg = "유저가 존재하지 않습니다.";
-  if(users.length > 0){
-        msg = users.length+"명의 유저가 존재합니다.";
-    }
-        res.send({msg, result: users});
-        
-});
-
-app.get("/users/:id",(req,res) => {
-    let msg = "id가 "+req.params.id+"인 유저가 존재하지 않습니다.";
-    let user = _.find(users,["id",parseInt(req.params.id)]);
-    if(user){
-        msg = "성공적으로 조회하였습니다.";
-    }
-   
-    res.send({msg,result: user});
-});
-
-app.post("/users",(req,res) => {
-    const check_user = _.find(users, ["id", req.body.id]);
-    let msg = req.body.id+"아이디를 가진 유저가 이미 존재합니다.";
-    let success = false;
-    if(!check_user){
-        users.push(req.body);
-        msg = req.body.name+"유저가 추가되었습니다.";
-        success = true;
-    }
-    res.send({msg, success});
-});
-
-app.put("/users/:id",(req,res) => {
-    if(user && user.id == req.params.id){
-    user.name = req.body.name;
-    res.send("user name" +user.name+" edit");
-    }
-    res.send("user id" +req.params.id+"가 존재하지 않습니다.");
-});
-
-app.delete("/users/:id",(req,res) => {
-    if(user && user.id == req.params.id){
-        user =null;
-        res.send("user" +req.params.id+" delete");
-    }else{
-        res.send("user id "+req.params.id+"가 존재하지 않습니다");
-    }
-  
-    
-  
+app.get("/", (req, res) => {
+    res.send("test");
 });
